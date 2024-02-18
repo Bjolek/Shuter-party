141f9bc92af64b60335ce5ad20fe564cdc4fd343
vadimzhenchur <152019836+Picok228@users.noreply.github.com>
2024-02-18 18:26:30 +0200

    урок 2

--- Player1.py
+++ Player1.py
@@ -16,13 +16,14 @@ class Player1:
     def move(self):
         keys = pygame.key.get_pressed()
 
+
         if keys[pygame.K_d]:
-            self.hit_box.x += self.speed
+            self.hit_box.y -= self.speed
         if keys[pygame.K_a]:
             self.hit_box.x -= self.speed
         if keys[pygame.K_w]:
-            self.hit_box.y += self.speed
+            self.hit_box.x += self.speed
         if keys[pygame.K_s]:
-            self.hit_box.y -= self.speed
+            self.hit_box.y += self.speed
 
 
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-
 import pygame
+import Player1
+import Player2
 
 
-window = pygame.display.set_mode((800, 500))
+
+PL1 = Player1.Player1(0, 700, 30, 50, 10, "Player1.png")
+PL2 = Player2.Player2(0, 100, 30, 50, 10, "Player2.png")
+
+
+
+window = pygame.display.set_mode((800, 800))
 fps = pygame.time.Clock()
 
+game = True
+while game:
 
-while True:
-
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+            pygame.quit()
+    window.fill((204,204,255))
+    PL1.render(window)
+    PL1.move()
+    PL2.render(window)
+    PL2.move()
     pygame.display.flip()
     fps.tick(60)
